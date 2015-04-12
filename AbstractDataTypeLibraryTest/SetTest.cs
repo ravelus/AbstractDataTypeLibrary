8fc2576f2ac2bf231cfaeb6b58b2c2cf7ceadd0a
luis.rodriguez.neches@outlook.com <luis.rodriguez.neches@outlook.com>
2015-04-12 11:05:36 -0300

    Rename dir

---
+++ AbstractDataTypeLibraryTest/SetTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TAD_Test
+{
+    [TestClass]
+    public class SetTest
+    {
+        //TODO
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AbstractDataTypeLibraryTest/SortingTests/QuickSorterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TAD_Library.Sorting;
+
+namespace TAD_Test.SortingTests
+{
+    [TestClass]
+    public class QuickSorterTest
+    {
+        //TODO
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
