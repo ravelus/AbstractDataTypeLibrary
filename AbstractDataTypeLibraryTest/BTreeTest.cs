0f93e49d244d8e61f184724d1f403c54acfe2ab4
luis.rodriguez.neches@outlook.com <luis.rodriguez.neches@outlook.com>
2015-04-19 12:33:34 -0300

    Added a test for remove before reviewing the remove algorithm

--- AbstractDataTypeLibraryTest/BTreeTest.cs
+++ AbstractDataTypeLibraryTest/BTreeTest.cs
@@ -101,5 +101,55 @@ namespace AbstractDataTypeLibraryTest
             Assert.IsTrue(treeTest.Contains(7));
             Assert.IsFalse(treeTest.Contains(8));
         }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            BTree<int> treeTest = new BTree<int>();
+
+            treeTest.Add(5);
+            treeTest.Add(3);
+            treeTest.Add(1);
+            treeTest.Add(4);
+            treeTest.Add(8);
+            treeTest.Add(7);
+            treeTest.Add(9);
+
+            treeTest.Remove(1);
+            Assert.AreEqual(5, treeTest.Head.Value);
+            Assert.AreEqual(3, treeTest.Head.Left.Value);
+            Assert.AreEqual(4, treeTest.Head.Left.Right.Value);
+            Assert.AreEqual(null, treeTest.Head.Left.Left);
+            Assert.AreEqual(8, treeTest.Head.Right.Value);
+            Assert.AreEqual(7, treeTest.Head.Right.Left.Value);
+            Assert.AreEqual(9, treeTest.Head.Right.Right.Value);
+
+            treeTest.Remove(8);
+            Assert.AreEqual(5, treeTest.Head.Value);
+            Assert.AreEqual(3, treeTest.Head.Left.Value);
+            Assert.AreEqual(4, treeTest.Head.Left.Right.Value);
+            Assert.AreEqual(null, treeTest.Head.Left.Left);
+            Assert.AreEqual(9, treeTest.Head.Right.Value);
+            Assert.AreEqual(7, treeTest.Head.Right.Left.Value);
+            Assert.AreEqual(null, treeTest.Head.Right.Right);
+
+            treeTest.Remove(5);
+            Assert.AreEqual(7, treeTest.Head.Value);
+            Assert.AreEqual(3, treeTest.Head.Left.Value);
+            Assert.AreEqual(4, treeTest.Head.Left.Right.Value);
+            Assert.AreEqual(null, treeTest.Head.Left.Left);
+            Assert.AreEqual(9, treeTest.Head.Right.Value);
+            Assert.AreEqual(null, treeTest.Head.Right.Left);
+            Assert.AreEqual(null, treeTest.Head.Right.Right);
+
+            treeTest.Remove(3);
+            Assert.AreEqual(7, treeTest.Head.Value);
+            Assert.AreEqual(4, treeTest.Head.Left.Value);
+            Assert.AreEqual(null, treeTest.Head.Left.Right);
+            Assert.AreEqual(null, treeTest.Head.Left.Left);
+            Assert.AreEqual(9, treeTest.Head.Right.Value);
+            Assert.AreEqual(null, treeTest.Head.Right.Left);
+            Assert.AreEqual(null, treeTest.Head.Right.Right);
+        }
     }
 }
